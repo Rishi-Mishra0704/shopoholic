40f8855b90a053b60405b1ff471b21779a9bf6a1
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-15 01:08:34 +0530

    add meals on screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
-class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+import 'package:shopoholic/models/meal.dart';
 
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+  final String title;
+  final List<Meal> meals;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'No meals found!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try changing your filters.',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) {
+          return Text(meals[index].title);
+        },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
