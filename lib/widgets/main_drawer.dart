24d5e193cee5dc43cae83c8bcd4cd7bb042ad13d
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-29 02:05:03 +0530

    add close function to side drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen});
 
+  final void Function(String id) onSelectScreen;
+ 
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -53,7 +55,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('meals');
+            },
           ),
           ListTile(
             leading: Icon(
@@ -68,7 +72,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('filters');
+            },
           ),
         ],
       ),
