1f2f3b8cb6acd76c8147fc5910d115f2a147ae55
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-30 00:34:27 +0530

    add animation to fav button

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -33,7 +33,22 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: Tween<double>(
+                      begin: 0.5,
+                      end: 1,
+                    ).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ),
             ),
           ],
         ),
